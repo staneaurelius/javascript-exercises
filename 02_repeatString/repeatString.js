89f4ee1c83179e2bfca4bce1e00d214e000a8054
staneaurelius <staneaurelius@gmail.com>
2024-07-23 20:15:07 +0700

    Complete JS exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = '';
+    if (num >= 0) {
+        for (let i=0; i<num; i++) {
+            repeatedString += string;
+        };
+    } else {
+        repeatedString = 'ERROR'
+    };
+    return repeatedString;
 };
 
 // Do not edit below this line
