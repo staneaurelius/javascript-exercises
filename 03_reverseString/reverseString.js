320246af41c54559d4c27f862e344021401fb509
staneaurelius <staneaurelius@gmail.com>
2024-07-23 20:29:06 +0700

    Complete JS exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let characters = word.split('').reverse();
+    return characters.join('');
 };
 
 // Do not edit below this line
