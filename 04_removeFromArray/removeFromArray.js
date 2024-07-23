166c9a72c51ea442d08bc25e7cd7854b97fb5136
staneaurelius <staneaurelius@gmail.com>
2024-07-23 21:59:51 +0700

    Complete JS exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(elementArray, ...elements) {
+    // Iterate over all values to be removed
+    for (let i = 0; i < elements.length; i++) {
+        // Keep removing the same element if multipel values of it exist
+        while (elementArray.includes(elements[i])) {
+            // To remove elements, find its index and slice it from the array
+            let elementIndex = elementArray.indexOf(elements[i]);
+            elementArray.splice(elementIndex, 1);
+        };
+    };
+    return elementArray;
 };
 
 // Do not edit below this line
