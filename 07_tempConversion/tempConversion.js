213935ddb4336b6279cbfb5a7f916cf0d2e585c9
staneaurelius <staneaurelius@gmail.com>
2024-07-24 22:38:39 +0700

    Complete JS exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegree) {
+  const celsiusDegree = (fahrenheitDegree - 32) * 5 / 9;
+  let roundedCelsiusDegree = Math.round(celsiusDegree * 10) / 10;
+  return roundedCelsiusDegree;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegree) {
+  const fahrenheitDegree = celsiusDegree * 9 / 5 + 32;
+  let roundedfahrenheitDegree = Math.round(fahrenheitDegree * 10) / 10
+  return roundedfahrenheitDegree;
 };
 
 // Do not edit below this line
