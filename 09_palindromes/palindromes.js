916188d8feee1091d4c287f3920322ce3b881575
staneaurelius <staneaurelius@gmail.com>
2024-07-28 19:21:06 +0800

    Complete JS exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    punctuations = [',', '.', '!', '?', ' '];
+    const filteredSentence = sentence
+        .toLowerCase()
+        .split('')
+        .filter((character) => !punctuations.includes(character))
+        .join('');
+    const reversedSentence = filteredSentence.split('').reverse().join('');
+    return filteredSentence === reversedSentence;
 };
 
 // Do not edit below this line
