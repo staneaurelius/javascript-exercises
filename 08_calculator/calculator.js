7da1586eef06eac34fbcfcd84ffbef151c255662
staneaurelius <staneaurelius@gmail.com>
2024-07-28 18:54:11 +0800

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+	return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(number, subtractor) {
+	return number - subtractor;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	return numberArray.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  return numberArray.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function(number, powerNumber) {
+	return number ** powerNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+	for (let i=0; i<number; i++) {
+    result *= i+1;
+  };
+  return result;
 };
 
 // Do not edit below this line
