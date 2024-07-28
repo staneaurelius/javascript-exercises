7eafbe36bea1cbdf7b027e89821c54ff584ef2fd
staneaurelius <staneaurelius@gmail.com>
2024-07-28 20:54:45 +0800

    Complete JS exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleAges = people.map(
+        (person) => person.yearOfDeath ?
+            person.yearOfDeath - person.yearOfBirth :
+            new Date().getFullYear() - person.yearOfBirth
+        );
+        
+    const oldestAgeIndex = peopleAges.indexOf(
+        Math.max(...peopleAges)
+    );
+    
+    return people[oldestAgeIndex];
 };
 
 // Do not edit below this line
