e8672f4a553108c983c5fd73b6c63fc945a1d282
staneaurelius <staneaurelius@gmail.com>
2024-07-28 20:21:06 +0800

    Complete JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fibonacciArray = [0, 1];
 
+    if (index > 1) {
+        for (let i = 1; i < index; i++) {
+            nextSequence = fibonacciArray.slice(-2).reduce((total, number) => total + number);
+            fibonacciArray.push(nextSequence);
+        }
+    } else if (index < 0) {
+        return 'OOPS'
+    };
+
+    return fibonacciArray[index];
 };
 
 // Do not edit below this line
